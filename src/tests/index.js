9886ea026a6e305b1c71fc2fdf44ea9ade33ed7d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-04-26 03:17:59 +0530

    Update mount tests.

--- src/tests/index.js
+++ src/tests/index.js
@@ -14,13 +14,7 @@ describe('Button', () => {
 
   it('should handle the click event', () => {
     const clickMe = sinon.stub();
-    // Here we do a JSDOM render. So, that's why we need to
-    // wrap this with a div.
-    const wrapper = mount(
-      <div>
-        <Button onClick={ clickMe }>ClickMe</Button>
-      </div>
-    );
+    const wrapper = mount(<Button onClick={ clickMe }>ClickMe</Button>);
 
     wrapper.find('button').simulate('click');
     expect(clickMe.callCount).to.be.equal(1);
