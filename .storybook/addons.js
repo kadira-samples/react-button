840f3b84c2809ee16e15fe2b0db37704bd2af96b
Muhammed Thanish <mnmtanish@gmail.com>
2016-09-29 16:07:53 +0530

    Add cloud database support

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,4 @@
 import '@kadira/storybook/addons';
-import '@kadira/storybook-addon-hello/register';
+import '@kadira/storybook-database-cloud/register';
 import '@kadira/storybook-addon-comments/register';
+import '@kadira/storybook-addon-hello/register';
